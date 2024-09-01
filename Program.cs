5a636722aeb531fb124ce895df3ed40e9b8fcf75
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-01 20:16:18 +0530

    matheod

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int firstvalue = 10;
+int secondvalue = 20;
+int largestvalue;
+if(firstvalue > secondvalue)
+{
+    largestvalue = firstvalue;
+}
+else
+{
+    largestvalue = secondvalue;
+}
+
+Console.WriteLine(largestvalue);
